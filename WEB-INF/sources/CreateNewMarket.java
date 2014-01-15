2248b440c21d83ce1b787046708e93dd2be8445f
Sinepel <constantin@constantin-blog.eu>
2014-01-15 21:02:01 +0100

    Servlet Create New Market

---
+++ WEB-INF/sources/CreateNewMarket.java
@@ -0,0 +1,23 @@
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+
+
+
+@WebServlet("/servlet/CreateNewMarket")
+public class CreateNewMarket extends HttpServlet
+{
+	public void doPost( HttpServletRequest req, HttpServletResponse res ) 
+		throws ServletException, IOException
+	{
+		
+		
+	}
+		
+	
+}
+
