343acd1f5c8e1afcd76bb8d02b344cf5975c62ed
florent-pulcian <florent.pulcian@gmail.com>
2014-01-28 18:59:01 +0100

    Gestion ajout ordre surcharge

--- WEB-INF/sources/informations/InformationDataBean.java
+++ WEB-INF/sources/informations/InformationDataBean.java
@@ -28,8 +28,8 @@ public class InformationDataBean{
 	
 	public InformationDataBean() throws Exception{
 		Class.forName("org.postgresql.Driver");
-		con = DriverManager.getConnection("jdbc:postgresql://localhost/lim","constantin","moi");
-		//con = DriverManager.getConnection("jdbc:postgresql://localhost/lim","postgres","postgres");
+		//con = DriverManager.getConnection("jdbc:postgresql://localhost/lim","constantin","moi");
+		con = DriverManager.getConnection("jdbc:postgresql://localhost/lim","postgres","postgres");
 		getInformation = con.prepareStatement("SELECT id, question, echeance, information.id_categorie, id_1, categorie.libelle FROM information,categorie WHERE information.id_categorie = categorie.id_categorie AND id = ?;");
 		getOrdresSql = con.prepareStatement("SELECT ordre.id_ordre, ordre.prix, ordre.nbbons, ordre.date_achat, ordre.id, ordre.user_id, utilisateur.pseudo, ordre.bonsRestants FROM ordre, utilisateur where ordre.user_id = utilisateur.user_id AND id = ? AND bonsRestants > 0 ORDER BY ordre.prix DESC;");
 		getOrdresInverseSql = con.prepareStatement("SELECT ordre.id_ordre, 100 - ordre.prix as prix, ordre.nbbons, ordre.date_achat, ordre.id, ordre.user_id, utilisateur.pseudo, ordre.bonsRestants FROM ordre, utilisateur where ordre.user_id = utilisateur.user_id AND id = ? AND bonsRestants > 0 ORDER BY ordre.prix ASC;");
@@ -125,8 +125,8 @@ public class InformationDataBean{
 	/**
 	 * Renvoyer la dernière clé générée, peut-être utile 
 	 **/
-	public void ajouterOrdre(int prix,int nbbons,int id,int user_id) throws Exception, SQLException{
-		
+	public void ajouterOrdre(int prix,int nbbons,int id,int user_id) throws Exception, SQLException
+	{		
 		ajoutOrdreSql.setInt(1,prix);
 		ajoutOrdreSql.setInt(2,nbbons);
 		//ajoutOrdreSql.setDate(3,date2);
@@ -135,7 +135,17 @@ public class InformationDataBean{
 		ajoutOrdreSql.setInt(5,nbbons);
 		
 		ajoutOrdreSql.executeUpdate();
+	}
+	
+	public void ajouterOrdre(int prix, int nbbons, int id, int user_id, int nbBonsRestants) throws Exception, SQLException
+	{
+		ajoutOrdreSql.setInt(1,prix);
+		ajoutOrdreSql.setInt(2,nbbons);
+		ajoutOrdreSql.setInt(3,id);
+		ajoutOrdreSql.setInt(4,user_id);
+		ajoutOrdreSql.setInt(5,nbBonsRestants);
 		
+		ajoutOrdreSql.executeUpdate();
 	}
 	
 	public void modifOrdre(int bonsARetirer, int ordre_id) throws Exception, SQLException
